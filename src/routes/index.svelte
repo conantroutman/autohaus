315fe000239a25f2902b1dd854aafd348e189fab
conantroutman <johsod96@gmail.com>
2022-05-29 03:14:54 +0200

    Wilkommen text

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<main>
+	<h1>Wilkommen bei Deutsche völklische Autohaus</h1>
+	<p>
+		Hier sehen Sie, welche Verkehrsmittel den Bürgern der Deutschen Demokratischen Republik
+		angeboten werden.
+	</p>
+	<div class="hint">
+		<p>Bitte geben Sie einen Befehl zum Navigieren ein.</p>
+		<p>Wenn Sie Hilfe benötigen, geben Sie den Befehl "hilfe" ein.</p>
+	</div>
+</main>
+
+<style>
+	.hint {
+		margin-top: 20px;
+	}
+</style>
