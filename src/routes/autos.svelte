9c0b0f361454e5930bf6809a88b0590ccffcbc06
conantroutman <johsod96@gmail.com>
2022-05-29 03:22:04 +0200

    Aufwiedersehen padding

--- src/routes/autos.svelte
+++ src/routes/autos.svelte
@@ -1,29 +1,27 @@
 <script>
-    import carsData from '../lib/data/autos.json';
-
+	import carsData from '../lib/data/autos.json';
 </script>
 
-
 <div class="auto-selector">
-    Bitte wählen Sie das Auto aus, das Sie sich ansehen möchten.
-    
-    <ul>
-        {#each carsData as carData, index}
-        <li>
-            [{index + 1}] {carData.brand} {carData.model}
-        </li>
-        {/each}
-    </ul>
+	Bitte wählen Sie das Auto aus, das Sie sich ansehen möchten.
 
-
-
+	<ul>
+		{#each carsData as carData, index}
+			<li>
+				[{index + 1}] {carData.brand}
+				{carData.model}
+			</li>
+		{/each}
+	</ul>
 </div>
-<style>
-    .auto-selector {
-        font-size: 30px;
-    }
 
-    ul {
-        list-style: none;
-    }
+<style>
+	.auto-selector {
+		font-size: 30px;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+	}
 </style>
