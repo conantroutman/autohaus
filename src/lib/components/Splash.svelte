8d66bdeb4f6224bcfcc777a5fbd19a3a6867c708
conantroutman <johsod96@gmail.com>
2022-05-29 03:22:17 +0200

    DDR

--- src/lib/components/Splash.svelte
+++ src/lib/components/Splash.svelte
@@ -1,13 +1,20 @@
 <div class="wrapper">
-	<h1>Autohaus</h1>
+	<div class="splash">
+		<img src="./img/ddr-logo.png" alt="" />
+		<h1>Autohaus</h1>
+	</div>
 </div>
 
 <style>
+	img {
+		height: 200px;
+		widows: 200px;
+		margin: auto;
+	}
+
 	h1 {
 		font-size: 10em;
-		animation-name: animateLogo;
-		animation-duration: 0.5s;
-		animation-delay: 0.1s;
+		margin: 0;
 	}
 
 	.wrapper {
@@ -17,6 +24,17 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+	}
+
+	.splash {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		animation-name: animateLogo;
+		animation-duration: 0.5s;
+		animation-delay: 0.1s;
 	}
 
 	@keyframes animate {
