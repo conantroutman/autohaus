8c294b301b85f1457b8cb1ada7240c194305f4ce
conantroutman <johsod96@gmail.com>
2022-05-29 02:41:47 +0200

    zurück history

--- src/lib/components/CommandLine.svelte
+++ src/lib/components/CommandLine.svelte
@@ -1,119 +1,105 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-    import { onMount } from 'svelte';
-    import { userCommands} from '$lib/stores'
-    import { page } from '$app/stores';
-    import carsData from '../../lib/data/autos.json';
-
+	import { onMount } from 'svelte';
+	import { userCommands } from '$lib/stores';
+	import { page } from '$app/stores';
+	import carsData from '../../lib/data/autos.json';
+	import { goBack } from '$lib/utils/navigate';
 
 	enum Commands {
 		GO_BACK = 'zurück',
 		HELP = 'hilfe',
-        DISPLAY_AUTOS = 'autos',
-        TEST = 'test',
-        CLEAR = 'klar'
+		DISPLAY_AUTOS = 'autos',
+		TEST = 'test',
+		CLEAR = 'klar'
 	}
 
 	let isFocused = false;
 	let input: any;
 
 	const onFocus = () => (isFocused = true);
-	const onBlur = () => (
-        input.focus()
-    );
+	const onBlur = () => input.focus();
 	const onChange = () => {
 		if (!input.textContent) return;
 		input.textContent = input.textContent?.toLocaleUpperCase();
 	};
 
-
-    function isNumeric(val:any) {
-        return /^-?\d+$/.test(val);
-    }
+	function isNumeric(val: any) {
+		return /^-?\d+$/.test(val);
+	}
 
 	const onKeypress = (e: KeyboardEvent) => {
-        input.focus();
+		input.focus();
 		if (e.key !== 'Enter') return;
 
-        e.preventDefault();
+		e.preventDefault();
 
-        userCommands.update((commands: string[]) => {
-            if (!input.textContent) return commands
-            return [...commands, input.textContent]
-        })
+		userCommands.update((commands: string[]) => {
+			if (!input.textContent) return commands;
+			return [...commands, input.textContent];
+		});
 
-        if (isNumeric(input.textContent) && $page.routeId === 'autos') {
-            
-            
+		if (isNumeric(input.textContent) && $page.routeId === 'autos') {
+			if (carsData[parseInt(input.textContent) - 1]) {
+				let carId = carsData[parseInt(input.textContent) - 1].id;
+				goto('/auto/' + carId);
+			} else {
+				logCommand('Das Auto, das Sie auswählen möchten, existiert nicht');
+			}
 
-            if (carsData[parseInt(input.textContent)-1]) {
-                let carId = carsData[parseInt(input.textContent)-1].id;
-                goto('/auto/'+carId);
-            } else {
-                logCommand('Das Auto, das Sie auswählen möchten, existiert nicht');
-            }
-
-            input.textContent = '';
-            return;
-
-        }
-
+			input.textContent = '';
+			return;
+		}
 
 		switch (input.textContent) {
 			case Commands.GO_BACK:
-				goto('/');
-                input.textContent = '';
+				window.history.back();
+				input.textContent = '';
 				return;
 
-            case Commands.DISPLAY_AUTOS:
-                goto('/autos');
-                input.textContent = '';
-                return;
-            
-            case Commands.TEST:
-                logCommand('Testbefehl');
-                input.textContent = '';
-                return;
+			case Commands.DISPLAY_AUTOS:
+				goto('/autos');
+				input.textContent = '';
+				return;
 
-            case Commands.HELP:
-                goto('/hilfe');
-                input.textContent = '';
-                return;
+			case Commands.TEST:
+				logCommand('Testbefehl');
+				input.textContent = '';
+				return;
 
-            case Commands.CLEAR:
-                userCommands.set([]);
-                input.textContent = '';
-                
-                return;
+			case Commands.HELP:
+				goto('/hilfe');
+				input.textContent = '';
+				return;
+
+			case Commands.CLEAR:
+				userCommands.set([]);
+				input.textContent = '';
+
+				return;
 
 			default:
-                logCommand('Fehler: unbekannte oder unvollständige Befehlszeile.');
-                logCommand('Verwenden Sie "Hilfe" für die richtigen Befehle');
-                return;
-          
+				logCommand('Fehler: unbekannte oder unvollständige Befehlszeile.');
+				logCommand('Verwenden Sie "Hilfe" für die richtigen Befehle');
+				return;
 		}
-
-
 	};
 
-    function logCommand(command:any) {
-        userCommands.update((commands: string[]) => {
-            input.textContent = '';
-            return [...commands, command]
-        })
-    }
-  
+	function logCommand(command: any) {
+		userCommands.update((commands: string[]) => {
+			input.textContent = '';
+			return [...commands, command];
+		});
+	}
 
-    onMount(async () => {
-        document.onclick = function(){
-        input.textContent = '';
-        input.focus();
-
-        }
+	onMount(async () => {
+		document.onclick = function () {
+			input.textContent = '';
+			input.focus();
+		};
 	});
 
-    onMount(() => input.focus())
- 
+	onMount(() => input.focus());
 </script>
 
 <div class="input-wrap">
@@ -122,17 +108,17 @@
 	<div class="user-input">
 		<span
 			class="user-input-field"
-            id="user-commandline-input"
+			id="user-commandline-input"
 			on:focus={onFocus}
 			on:blur={onBlur}
 			on:keypress={onKeypress}
 			contenteditable="true"
 			bind:this={input}
-            autofocus
-            spellcheck="false"></span
-		>
+			autofocus
+			spellcheck="false"
+		/>
 
-        <div class="cursor-block" />
+		<div class="cursor-block" />
 	</div>
 </div>
 
@@ -154,8 +140,8 @@
 	.user-input-field {
 		background: transparent;
 		border: none;
-        caret-color: transparent;     
-        max-height: 30px;   
+		caret-color: transparent;
+		max-height: 30px;
 	}
 
 	.user-input-field:focus {
@@ -203,5 +189,4 @@
 	input:focus {
 		outline: none;
 	}
-    
 </style>
