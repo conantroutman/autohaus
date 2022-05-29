4682c9aa26291a7a8ed50cd072df316e02e79d14
conantroutman <johsod96@gmail.com>
2022-05-29 03:31:11 +0200

    BITTE HILFEN

--- src/routes/hilfe.svelte
+++ src/routes/hilfe.svelte
@@ -1,16 +1,39 @@
-<h2>Diese Befehle können zum Navigieren verwendet werden</h2>
+<main>
+	<h2>Diese Befehle können zum Navigieren verwendet werden</h2>
 
-<ul>
-    <li>- autos: verfügbare Autos anzeigen</li>
-    <li>- hielfen: Holen Sie sich Hilfe bei Befehlen    </li>
-    <li>- zurück: geh zurück</li>
-    <li>- klar: Löschen Sie den Befehlsverlauf</li>
-  </ul>
+	<ul>
+		<li>
+			<span class="command">AUTOS</span>
+			<span>Verfügbare Autos anzeigen.</span>
+		</li>
+		<li>
+			<span class="command"> HILFE</span>
+			<span>Holen Sie sich Hilfe bei Befehlen.</span>
+		</li>
+		<li>
+			<span class="command">ZURÜCK</span>
+			<span>Geh zurück.</span>
+		</li>
+		<li>
+			<span class="command">KLAR</span>
+			<span>Löschen Sie den Befehlsverlauf.</span>
+		</li>
+	</ul>
+</main>
 
+<style>
+	ul {
+		list-style: none;
+		font-size: 30px;
+		padding: 0;
+	}
 
-  <style>
-        ul {
-        list-style: none;
-        font-size: 30px;
-        }
-  </style>
+	li {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.command {
+		width: 10rem;
+	}
+</style>
